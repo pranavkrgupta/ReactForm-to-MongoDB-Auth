bc249f9e75e81567d252a844e9549a4c3bfabeb5
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2024-05-26 07:09:31 +0530

    few changes for nav, signin, signup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Feedback from './components/Feedback';
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Navbar from './components/Navbar';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 const App = () => {
   return (
-    <div className="">
-      <Feedback/>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
 
-export default App
+  );
+};
+
+export default App;
--- Client/src/components/Feedback.jsx
+++ Client/src/components/Feedback.jsx
@@ -51,7 +51,7 @@ const Feedback = () => {
                 showForm: false,
                 showPopup: true,
             });
-            
+
             // Optionally, you can reset the form after successful submission
             setFormData({
                 name: '',
@@ -76,72 +76,81 @@ const Feedback = () => {
     return (
         <>
             {handleFeedback.showForm && (
-                <>
-                    <h2 className='text-center'>We Value Your Feedback</h2>
+                <section className="bg-light min-vh-100 d-flex align-items-center justify-content-center">
                     <div className="container">
-                        <form onSubmit={handleSubmit}>
-                            <div className="mb-3">
-                                <label htmlFor="name" className="form-label">Enter Your Full Name:</label>
-                                <input className="form-control" type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
-                            </div>
+                        <div className="row justify-content-center">
+                            <div className="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                                <div className="card border border-light-subtle rounded-3 shadow">
+                                    <div className="card-body p-3 p-md-4 p-xl-5">
+                                        <h2 className='card-title text-center'>We Value Your Feedback</h2>
+                                        <form onSubmit={handleSubmit}>
+                                            <div className="mb-3">
+                                                <label htmlFor="name" className="form-label">Enter Your Full Name:</label>
+                                                <input className="form-control" type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+                                            </div>
 
-                            <div className="mb-3">
-                                <label htmlFor="email" className="form-label">Email:</label>
-                                <input className="form-control" type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
-                            </div>
+                                            <div className="mb-3">
+                                                <label htmlFor="email" className="form-label">Email:</label>
+                                                <input className="form-control" type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+                                            </div>
 
-                            <div className="mb-3">
-                                <label className="form-label">Is this your first time visiting the website?</label>
-                                <div className="form-check">
-                                    <input type="radio" id="firstVisitYes" name="firstVisit" className="form-check-input" value="Yes" onChange={handleChange} />
-                                    <label htmlFor="firstVisitYes" className="form-check-label">Yes</label>
+                                            <div className="mb-3">
+                                                <label className="form-label">Is this your first time visiting the website?</label>
+                                                <div className="form-check">
+                                                    <input type="radio" id="firstVisitYes" name="firstVisit" className="form-check-input" value="Yes" onChange={handleChange} />
+                                                    <label htmlFor="firstVisitYes" className="form-check-label">Yes</label>
+                                                </div>
+                                                <div className="form-check">
+                                                    <input type="radio" id="firstVisitNo" name="firstVisit" className="form-check-input" value="No" onChange={handleChange} />
+                                                    <label htmlFor="firstVisitNo" className="form-check-label">No</label>
+                                                </div>
+                                            </div>
+
+                                            <div className="mb-3">
+                                                <label className="form-label">Did you find what you needed?</label>
+                                                <div className="form-check">
+                                                    <input type="radio" id="foundAll" name="found" value="All" className="form-check-input" onChange={handleChange} />
+                                                    <label htmlFor="foundAll" className="form-check-label">Yes, All of it.</label>
+                                                </div>
+                                                <div className="form-check">
+                                                    <input type="radio" id="foundSome" name="found" value="Some" className="form-check-input" onChange={handleChange} />
+                                                    <label htmlFor="foundSome" className="form-check-label">Yes, Some of it.</label>
+                                                </div>
+                                                <div className="form-check">
+                                                    <input type="radio" id="foundNone" name="found" value="None" className="form-check-input" onChange={handleChange} />
+                                                    <label htmlFor="foundNone" className="form-check-label">No, None of it.</label>
+                                                </div>
+                                            </div>
+
+                                            <div className="mb-3">
+                                                <label htmlFor="">Please rate the quality of our product/service</label>
+                                                <StarRating rating={formData.qualityRating} setRating={handleQualityRatingChange} />
+                                            </div>
+
+                                            <div className="mb-3">
+                                                <label htmlFor="">How likely are you to recommend us to a friend or colleague?</label>
+                                                <StarRating rating={formData.recommendRating} setRating={handleRecommendRatingChange} />
+                                            </div>
+
+                                            <div className="mb-3">
+                                                <label className="form-label">Do you have any suggestions for how we can improve?</label>
+                                                <textarea className="form-control" name="suggestions" value={formData.suggestions} onChange={handleChange} placeholder="We'd love to hear your suggestions." />
+                                            </div>
+
+                                            <div className="mb-3">
+                                                <button type="submit" className="btn btn-primary">Submit</button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
-                                <div className="form-check">
-                                    <input type="radio" id="firstVisitNo" name="firstVisit" className="form-check-input" value="No" onChange={handleChange} />
-                                    <label htmlFor="firstVisitNo" className="form-check-label">No</label>
-                                </div>
                             </div>
-
-                            <div className="mb-3">
-                                <label className="form-label">Did you find what you needed?</label>
-                                <div className="form-check">
-                                    <input type="radio" id="foundAll" name="found" value="All" className="form-check-input" onChange={handleChange} />
-                                    <label htmlFor="foundAll" className="form-check-label">Yes, All of it.</label>
-                                </div>
-                                <div className="form-check">
-                                    <input type="radio" id="foundSome" name="found" value="Some" className="form-check-input" onChange={handleChange} />
-                                    <label htmlFor="foundSome" className="form-check-label">Yes, Some of it.</label>
-                                </div>
-                                <div className="form-check">
-                                    <input type="radio" id="foundNone" name="found" value="None" className="form-check-input" onChange={handleChange} />
-                                    <label htmlFor="foundNone" className="form-check-label">No, None of it.</label>
-                                </div>
-                            </div>
-
-                            <div className="mb-3">
-                                <label htmlFor="">Please rate the quality of our product/service</label>
-                                <StarRating rating={formData.qualityRating} setRating={handleQualityRatingChange} />
-                            </div>
-
-                            <div className="mb-3">
-                                <label htmlFor="">How likely are you to recommend us to a friend or colleague?</label>
-                                <StarRating rating={formData.recommendRating} setRating={handleRecommendRatingChange} />
-                            </div>
-
-                            <div className="mb-3">
-                                <label className="form-label">Do you have any suggestions for how we can improve?</label>
-                                <textarea className="form-control" name="suggestions" value={formData.suggestions} onChange={handleChange} placeholder="We'd love to hear your suggestions." />
-                            </div>
-
-                            <div className="mb-3">
-                                <button type="submit" className="btn btn-primary">Submit</button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
-                </>
-            )}
+                </section>
+    )
+}
 
-            {handleFeedback.showPopup && <Popup message="Form submission successful!" onClose={() => setHandleFeedback({ showForm: true, showPopup: false })} />}
+{ handleFeedback.showPopup && <Popup message="Form submission successful!" onClose={() => setHandleFeedback({ showForm: true, showPopup: false })} /> }
         </>
 
     );
